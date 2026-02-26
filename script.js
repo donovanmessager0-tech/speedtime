c71328e664d5913cc07b725b43f6b2ccccaadbc1
donovanmessager0-tech <donovanmessager0@gmail.com>
2026-02-26 13:25:35 +0000

    ajout d;un event listener pour duration

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const duration = document.querySelector("#duration");
+
+btnstart.addEventListener('inputs', () => console.log('input'));
+
+
 const btnstart = document.querySelector("#btn-start");
 
 btnstart.addEventListener('click', () => console.log('click'));
