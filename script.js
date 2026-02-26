5c5fd611e818d0a1709b2c1c32bbf4e7a28f8352
donovanmessager0-tech <donovanmessager0@gmail.com>
2026-02-26 13:49:47 +0000

    ajout du showmodal

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const duration = document.querySelector("#duration");
 
-btnstart.addEventListener('inputs', () => console.log('input'));
+duration.addEventListener('inputs', () => console.log('input'));
 
 
 const btnstart = document.querySelector("#btn-start");
 
 btnstart.addEventListener('click', () => console.log('click'));
+
+const words = document.querySelector('#words-display')
+
+words.showmodal()
